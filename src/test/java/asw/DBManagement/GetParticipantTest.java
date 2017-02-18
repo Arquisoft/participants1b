3a6e9db66ba7f32aa3b67da493d2287fcd54186b
Álex Sáez <alexsaezm@gmail.com>
2017-02-18 16:04:05 +0100

    Trying something

--- src/test/java/asw/DBManagement/GetParticipantTest.java
+++ src/test/java/asw/DBManagement/GetParticipantTest.java
@@ -1,6 +1,39 @@
 package asw.DBManagement;
 
+import static org.mockito.Mockito.when;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 public class GetParticipantTest {
+
+	// private asw.DBManagement.GetParticipant getParticipant;
+	private asw.DBManagement.model.Ciudadano johnDoe;
+
+	@Autowired
+	private asw.DBManagement.persistence.CiudadanoRepository repository;
+
+	@Before
+	public void setUp() {
+		Date bornDate = new Date();
+
+		try {
+			bornDate = new SimpleDateFormat("yyyy-MM-dd").parse("1970-01-01");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		johnDoe = new asw.DBManagement.model.Ciudadano("John", "Doe", "john@doe.net", bornDate, "Phobos", "Martian",
+				"123456789", "password");
+	}
+
+	@Test
+	public void testgetCiudadano() {
+		when(repository.save(johnDoe)).thenReturn(johnDoe);
+	}
 
 }
