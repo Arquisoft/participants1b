bc48ef4ce371c787a54bb41ffcbf25c0dd1c2cad
Álex Sáez <alexsaezm@gmail.com>
2017-02-21 01:26:06 +0100

    New tests

---
+++ src/test/java/asw/participants/acceso/ChangePasswordTest.java
@@ -0,0 +1,54 @@
+package asw.participants.acceso;
+
+import asw.Application;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@Transactional
+public class ChangePasswordTest {
+
+    private ChangePassword changePassword;
+
+    private String email;
+    private String password;
+    private String newPassword;
+
+
+    @Before
+    public void setUp() throws Exception {
+        email = "email@email.net";
+        password = "oldPassword";
+        newPassword = "newPassword";
+        changePassword = new ChangePassword(email,password,newPassword);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getPassword() throws Exception {
+        assertThat(changePassword.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    public void getEmail() throws Exception {
+        assertThat(changePassword.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    public void getNewPassword() throws Exception {
+        assertThat(changePassword.getNewPassword()).isEqualTo(newPassword);
+    }
+}
